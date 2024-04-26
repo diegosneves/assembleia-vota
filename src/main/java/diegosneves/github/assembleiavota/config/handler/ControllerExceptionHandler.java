118317a041ef82be7a0d6bc447a9e2e81d09f10e
Diego Neves <neves.diegoalex@outlook.com>
2024-04-25 23:51:13 -0300

    feat/issue-14 :sparkles: Adiciona manipulador de exceções global e DTO de exceção
    Este commit inclui um manipulador de exceções global para controladores, que lida com as exceções lançadas ao processar solicitações e gera respostas de erro correspondentes. Juntamente com isso, um Data Transfer Object (DTO) também foi introduzido para encapsular a mensagem da exceção e o código do status HTTP.

---
+++ src/main/java/diegosneves/github/assembleiavota/config/handler/ControllerExceptionHandler.java
@@ -0,0 +1,32 @@
+package diegosneves.github.assembleiavota.config.handler;
+
+import diegosneves.github.assembleiavota.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * A classe {@link ControllerExceptionHandler} é um manipulador de exceções global para controladores.
+ * Ela lida com as exceções lançadas durante o processamento de solicitações e gera respostas de erro apropriadas.
+ * A classe é anotada com {@link RestControllerAdvice} para aplicar o tratamento de exceção globalmente
+ * a todas as classes de controlador.
+ *
+ * @author diegosneves
+ */
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    /**
+     * Manipula exceções gerais e retorna uma resposta de erro apropriada.
+     *
+     * @param exception A exceção que ocorreu.
+     * @return Uma {@link ResponseEntity} contendo um {@link ExceptionDTO} com a mensagem da exceção e um código de status HTTP
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> generalError(Exception exception) {
+        ExceptionDTO dto = new ExceptionDTO(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(dto);
+    }
+
+}
