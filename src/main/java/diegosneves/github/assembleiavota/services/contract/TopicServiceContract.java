aa7c86dd3a0959f77f59220a106a80b7bfd4ec7a
Diego Neves <neves.diegoalex@outlook.com>
2024-04-26 23:31:34 -0300

    feat/I16-23 :sparkles: Adiciona serviço para criar novos tópicos de discussão
    Esse commit introduz a implementação de um serviço para criar novos tópicos de discussão. Este serviço valida as entradas do usuário para garantir que sejam válidas antes de criar e persistir os dados do tópico. Além disso, extrai informações relevantes do tópico criado e as encapsula em uma resposta para o usuário.

---
+++ src/main/java/diegosneves/github/assembleiavota/services/contract/TopicServiceContract.java
@@ -0,0 +1,22 @@
+package diegosneves.github.assembleiavota.services.contract;
+
+import diegosneves.github.assembleiavota.exceptions.TopicAttributeException;
+import diegosneves.github.assembleiavota.requests.TopicRequest;
+import diegosneves.github.assembleiavota.responses.TopicCreatedResponse;
+
+
+public interface TopicServiceContract {
+
+    /**
+     * Cria um novo {@link diegosneves.github.assembleiavota.models.TopicEntity tópico}.
+     * Valida o tópico com base no título, descrição e duração da sessão de votação.
+     * Persiste o tópico validado no repositório.
+     * Retorna uma resposta contendo detalhes do tópico criado.
+     *
+     * @param topicRequest O objeto de solicitação de tópico contendo o título, a descrição e a duração da sessão de votação.
+     * @return uma instância de {@link TopicCreatedResponse} contendo detalhes do tópico criado.
+     * @throws TopicAttributeException em caso de título inválido, descrição ou duração da sessão de votação.
+     */
+    TopicCreatedResponse createNewTopic(TopicRequest topicRequest) throws TopicAttributeException;
+
+}
