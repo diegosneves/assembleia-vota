6d66512a0edabeb78bf9267e918b3efe6139d39c
Diego Neves <neves.diegoalex@outlook.com>
2024-04-28 20:08:09 -0300

    feat/I31-33 :sparkles: Adiciona implementação e testes para o serviço de Sessão
    Este commit adiciona a implementação do serviço SessionService e seu respectivo contrato SessionServiceContract. Além disso, inclui testes unitários para garantir o seu correto funcionamento. O SessionService é responsável por controlar as operações relacionadas às sessões de votação, como iniciar uma nova sessão, recuperar uma sessão existente e verificar se uma sessão está aberta.

---
+++ src/main/java/diegosneves/github/assembleiavota/services/SessionService.java
@@ -0,0 +1,124 @@
+package diegosneves.github.assembleiavota.services;
+
+import diegosneves.github.assembleiavota.exceptions.IllegalSessionArgumentException;
+import diegosneves.github.assembleiavota.exceptions.InvalidIdException;
+import diegosneves.github.assembleiavota.exceptions.SessionCreateFailureException;
+import diegosneves.github.assembleiavota.exceptions.SessionNotFound;
+import diegosneves.github.assembleiavota.exceptions.TopicIdNotFoundException;
+import diegosneves.github.assembleiavota.exceptions.UuidUtilsException;
+import diegosneves.github.assembleiavota.factory.SessionEntityFactory;
+import diegosneves.github.assembleiavota.mapper.MapperStrategy;
+import diegosneves.github.assembleiavota.mapper.SessionCreatedResponseMapper;
+import diegosneves.github.assembleiavota.models.SessionEntity;
+import diegosneves.github.assembleiavota.models.TopicEntity;
+import diegosneves.github.assembleiavota.repositories.SessionEntityRepository;
+import diegosneves.github.assembleiavota.requests.StartSessionRequest;
+import diegosneves.github.assembleiavota.responses.SessionCreatedResponse;
+import diegosneves.github.assembleiavota.services.contract.SessionServiceContract;
+import diegosneves.github.assembleiavota.services.contract.TopicServiceContract;
+import diegosneves.github.assembleiavota.utils.UuidUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+
+@Service
+@Slf4j
+public class SessionService implements SessionServiceContract {
+
+    public static final String NULL_TOPIC_ID = "ID da Pauta não pode ser nulo";
+    public static final String INVALID_TOPIC_ID = "ID da Pauta é inválida";
+    public static final String TOPIC_DOES_NOT_EXIST = "O ID da Pauta fornecido não foi encontrado no sistema. Certifique-se de que o ID está correto.";
+    public static final String SESSION_ID_CANNOT_BE_NULL = "da Sessão não pode ser nulo e";
+    public static final String SESSION_CANNOT_BE_NULL = "A Session não pode ser nula";
+
+    private final SessionEntityRepository repository;
+    private final TopicServiceContract topicService;
+
+    @Autowired
+    public SessionService(SessionEntityRepository repository, TopicServiceContract topicService) {
+        this.repository = repository;
+        this.topicService = topicService;
+    }
+
+    @Override
+    public SessionCreatedResponse startSession(StartSessionRequest request) throws SessionCreateFailureException {
+        TopicEntity topic = null;
+        try {
+            topic = this.topicService.getTopic(request.getTopicId());
+        } catch (InvalidIdException e) {
+            String message = request.getTopicId() == null ? NULL_TOPIC_ID : INVALID_TOPIC_ID;
+            log.error(SessionCreateFailureException.ERROR.getMessage(message), e);
+            throw new SessionCreateFailureException(message, e);
+        } catch (TopicIdNotFoundException e) {
+            log.error(SessionCreateFailureException.ERROR.getMessage(TOPIC_DOES_NOT_EXIST), e);
+            throw new SessionCreateFailureException(TOPIC_DOES_NOT_EXIST, e);
+        }
+        SessionEntity session = SessionEntityFactory.create(topic, LocalDateTime.now());
+        this.repository.save(session);
+        MapperStrategy<SessionCreatedResponse, SessionEntity> mapper = new SessionCreatedResponseMapper();
+        return mapper.mapFrom(session);
+    }
+
+    @Override
+    public SessionEntity getSession(String sessionId) throws SessionNotFound, InvalidIdException {
+        Optional<SessionEntity> optionalSession = this.repository.findBySessionId(validateSessionId(sessionId));
+        if (optionalSession.isPresent()) {
+            return optionalSession.get();
+        }
+        log.error(SessionNotFound.ERROR.getMessage(sessionId));
+        throw new SessionNotFound(sessionId);
+    }
+
+    /**
+     * Valida se a SessionId fornecida é válida. Ela não pode ser nula e deve ser um UUID válido.
+     *
+     * @param sessionId A identificação da sessão a ser validada.
+     * @return A session Id se ela for válida.
+     * @throws InvalidIdException se o sessionId for nulo ou se não for um UUID válido.
+     */
+    private static String validateSessionId(String sessionId) throws InvalidIdException {
+        if (sessionId == null) {
+            log.error(InvalidIdException.ERROR.getMessage(SESSION_ID_CANNOT_BE_NULL));
+            throw new InvalidIdException(SESSION_ID_CANNOT_BE_NULL);
+        }
+        try {
+            UuidUtils.isValidUUID(sessionId.trim());
+            return sessionId.trim();
+        } catch (UuidUtilsException e) {
+            log.error(InvalidIdException.ERROR.getMessage(sessionId), e);
+            throw new InvalidIdException(sessionId.trim(), e);
+        }
+    }
+
+    @Override
+    public boolean sessionIsOpen(SessionEntity session) throws IllegalSessionArgumentException {
+        sessionValidate(session);
+        boolean isOpen = LocalDateTime.now().isBefore(session.getEndTime());
+        session.setOpen(isOpen);
+        this.repository.save(session);
+        return isOpen;
+    }
+
+    /**
+     * Valida se a entidade da sessão fornecida é não-nula.
+     *
+     * @param session A entidade da sessão que será validada.
+     * @throws IllegalSessionArgumentException Se a entidade da sessão for nula.
+     */
+    private static void sessionValidate(SessionEntity session) throws IllegalSessionArgumentException {
+        if (isNull(session)) {
+            throw new IllegalSessionArgumentException(SESSION_CANNOT_BE_NULL);
+        }
+    }
+
+    @Override
+    public void updateSession(SessionEntity session) throws IllegalSessionArgumentException {
+        sessionValidate(session);
+        this.repository.save(session);
+    }
+}
---
+++ src/main/java/diegosneves/github/assembleiavota/services/contract/SessionServiceContract.java
@@ -0,0 +1,61 @@
+package diegosneves.github.assembleiavota.services.contract;
+
+import diegosneves.github.assembleiavota.exceptions.IllegalSessionArgumentException;
+import diegosneves.github.assembleiavota.exceptions.InvalidIdException;
+import diegosneves.github.assembleiavota.exceptions.SessionCreateFailureException;
+import diegosneves.github.assembleiavota.exceptions.SessionNotFound;
+import diegosneves.github.assembleiavota.models.SessionEntity;
+import diegosneves.github.assembleiavota.requests.StartSessionRequest;
+import diegosneves.github.assembleiavota.responses.SessionCreatedResponse;
+
+/**
+ * {@link SessionServiceContract} é uma interface que define o contrato para os serviços de sessão.
+ * Contêm a definição do método para iniciar uma nova sessão.
+ *
+ * @author diegoneves
+ */
+public interface SessionServiceContract {
+
+    /**
+     * Esse método é responsável por iniciar uma nova sessão baseada no objeto de solicitação fornecido.
+     *
+     * @param request objeto do tipo {@link StartSessionRequest} que contém as informações necessárias para iniciar uma nova sessão.
+     * @return retorna um objeto do tipo {@link SessionCreatedResponse} que indica a criação bem-sucedida da sessão e apresenta os detalhes da sessão recém-criada.
+     * @throws SessionCreateFailureException é lançada quando ocorre algum erro durante a criação da sessão.
+     */
+    SessionCreatedResponse startSession(StartSessionRequest request) throws SessionCreateFailureException;
+
+
+    /**
+     * Este método é utilizado para recuperar uma sessão a partir de um ID de sessão fornecido.
+     * Primeiramente, o método realiza a validação do ID da sessão e depois tenta recuperar a sessão do repositório.
+     * Caso a sessão não seja encontrada, um erro é registrado no log e uma exceção é lançada.
+     *
+     * @param sessionId ID da sessão a ser recuperada
+     * @return Retorna uma instância de {@link SessionEntity} que corresponde ao ID da sessão informado
+     * @throws SessionNotFound    Exceção lançada caso a sessão com o ID fornecido não seja encontrada
+     * @throws InvalidIdException Exceção lançada caso o ID da sessão fornecido não esteja em um formato válido
+     */
+    SessionEntity getSession(String sessionId) throws SessionNotFound, InvalidIdException;
+
+    /**
+     * Verifica se uma sessão está aberta.
+     * O método faz isso ao comparar se a hora atual é anterior ao horário de término da sessão.
+     * Após isso, o estado da sessão é atualizado com o resultado da verificação e a
+     * sessão modificada é salva no repositório.
+     *
+     * @param session A sessão a ser verificada. Não pode ser {@code null}.
+     * @return {@code true} se a sessão estiver aberta; {@code false} caso contrário.
+     * @throws IllegalSessionArgumentException Se a sessão passada como argumento for inválida.
+     */
+    boolean sessionIsOpen(SessionEntity session) throws IllegalSessionArgumentException;
+
+    /**
+     * Atualiza uma sessão existente na base de dados.
+     *
+     * @param session A sessão a ser atualizada.
+     * @throws IllegalSessionArgumentException se a sessão for inválida.
+     */
+    void updateSession(SessionEntity session) throws IllegalSessionArgumentException;
+
+}
